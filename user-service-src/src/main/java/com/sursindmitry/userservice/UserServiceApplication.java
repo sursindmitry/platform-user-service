8028cd3b0ef458616731610736f3bb3db5cf6932
Dmitry Sursin <40328886+sursindmitry@users.noreply.github.com>
2024-08-25 18:55:12 +0700

    PRJ-104: Add checkstyle (#7) (#8)

--- user-service-src/src/main/java/com/sursindmitry/userservice/UserServiceApplication.java
+++ user-service-src/src/main/java/com/sursindmitry/userservice/UserServiceApplication.java
@@ -3,6 +3,9 @@ package com.sursindmitry.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class.
+ */
 @SpringBootApplication
 public class UserServiceApplication {
 
